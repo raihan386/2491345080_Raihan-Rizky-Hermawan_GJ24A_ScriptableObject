55497e777b9e154bc62e6a561f9ae25453aa2cbd
raihan386 <raihanrizky386@gmail.com>
2026-02-19 12:19:53 +0700

    Add files via upload

---
+++ Script/WayPoint/TwoPointPositionMover.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TwoPointPositionMover : MonoBehaviour
+{
+
+    public Route2PointSO route;
+    public float speed = 2f;
+
+    private Vector3 target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = route.pointA;
+        target = route.pointB;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position == target)
+        {
+            target = (target == route.pointA) ? route.pointB : route.pointA;
+        }
+    }
+}
