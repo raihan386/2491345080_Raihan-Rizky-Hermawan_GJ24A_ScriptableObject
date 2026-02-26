34be83a84d85be111751ced5197157afb581cef1
raihan386 <raihanrizky386@gmail.com>
2026-02-26 15:23:54 +0700

    Add files via upload

---
+++ Assets/Script/WayPoint/Route2PointSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Route2PointSO", menuName = "Scriptable Objects/Route2PointSO")]
+public class Route2PointSO : ScriptableObject
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+}
